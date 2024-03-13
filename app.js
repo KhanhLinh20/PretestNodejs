8d93c2d8926b9e085773815b0c1608e8ee74e6e1
KhanhLinh20 <129593513+KhanhLinh20@users.noreply.github.com>
2024-03-13 14:38:25 +0700

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use('/users', usersRouter);
 app.use('/customer', customerRouter);
 
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://127.0.0.1:27017/demo", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://khanhlinhdh98:khanhlinhdh98@cluster0.0xkxg1s.mongodb.net/", {useNewUrlParser: true});
 
 
 // catch 404 and forward to error handler
